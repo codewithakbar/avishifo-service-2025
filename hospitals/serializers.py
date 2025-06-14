fe89015603713731c7df8dd7e4eeaa7df2110c2b
Shohzod <satipovakbar@gmail.com>
2025-06-15 03:33:47 +0500

    asdfasdf

--- hospitals/serializers.py
+++ hospitals/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Hospital
 
+
 class HospitalSerializer(serializers.ModelSerializer):
     total_doctors = serializers.ReadOnlyField()
     available_doctors = serializers.ReadOnlyField()
@@ -8,8 +9,16 @@ class HospitalSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Hospital
-        fields = ['id', 'name', 'address', 'phone', 'phone_number', 'available_doctors']
-        read_only_fields = ('created_at', 'updated_at')
+        fields = [
+            "id",
+            "name",
+            "address",
+            "phone",
+            "phone_number",
+            "available_doctors",
+            "total_doctors",
+        ]
+        read_only_fields = ("created_at", "updated_at")
 
     def get_phone_number(self, obj):
         return obj.phone
@@ -19,7 +28,13 @@ class HospitalCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hospital
         fields = (
-            'name', 'address', 'phone_number', 'email', 'website',
-            'license_number', 'established_date', 'bed_capacity',
-            'emergency_services'
+            "name",
+            "address",
+            "phone_number",
+            "email",
+            "website",
+            "license_number",
+            "established_date",
+            "bed_capacity",
+            "emergency_services",
         )
