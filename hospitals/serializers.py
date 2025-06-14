e661faef52a0b65ad33641ce8319670221a653c2
Shohzod <satipovakbar@gmail.com>
2025-06-15 03:32:38 +0500

    asdf

--- hospitals/serializers.py
+++ hospitals/serializers.py
@@ -8,7 +8,7 @@ class HospitalSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Hospital
-        fields = ['id', 'name', 'address', 'phone', 'phone_number']
+        fields = ['id', 'name', 'address', 'phone', 'phone_number', 'available_doctors']
         read_only_fields = ('created_at', 'updated_at')
 
     def get_phone_number(self, obj):
